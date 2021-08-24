337d2f7a053df34d6746fdbcfcbd6b96bf0cd2d1
Igor Victor <contatoigorvictor@gmail.com>
2021-08-24 18:18:04 -0300

    Foram adicionadas função sleep e formatação

--- ex028.py
+++ ex028.py
@@ -1,10 +1,14 @@
 from random import randint
+from time import sleep
 print('Exercício 28: adivinhar número esolhido pelo PC.')
 # fazer computador escolher número
 numero = randint(0, 5)
-print('Escolhi um número entre 0 e 5! Que número eu escolhi?')
+print('PROCESSANDO...')
+sleep(2)
+print('Ok! Pensei em um número entre 0 e 5!')
+print('Que número eu escolhi?')
 n = int(input('Digite um número: '))
 if n != numero:
-    print('Que pena! Você errou!')
+    print('Que pena! Você errou! Eu pensei no número {}!'.format(numero))
 else:
     print('Parabéns! Você acertou!!!')
