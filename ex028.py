e6e904bcfee7d5766c7702fdccdb00af5a038b9e
Igor Victor <contatoigorvictor@gmail.com>
2021-08-24 12:07:31 -0300

    Reduzido o espaço amostral de números que o código pode escolher;

--- ex028.py
+++ ex028.py
@@ -1,8 +1,8 @@
 from random import randint
 print('Exercício 28: adivinhar número esolhido pelo PC.')
 # fazer computador escolher número
-numero = randint(1, 10)
-print('Escolhi um número de 1 a 10. Que número eu escolhi?')
+numero = randint(0, 5)
+print('Escolhi um número entre 0 e 5! Que número eu escolhi?')
 n = int(input('Digite um número: '))
 if n != numero:
     print('Que pena! Você errou!')
