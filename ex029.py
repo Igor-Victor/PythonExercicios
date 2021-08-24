07dcb977577d9e01f2335f3d6d4824bc48a30e6c
Igor Victor <contatoigorvictor@gmail.com>
2021-08-24 18:19:12 -0300

    Editada formatação da sring para duas casas decimais em float, para que se adeque a dinheiro.

--- ex029.py
+++ ex029.py
@@ -4,6 +4,6 @@ km = int(input('Qual é a velocidade do seu carro? '))
 multa = (km - 80) * 7
 if km > 80:
     print('Você ultrapassou o limite de velocidade por {}KM/H! '.format(km - 80))
-    print('Você pagará de multa por sua infração: R${}! '.format(multa))
+    print('Você pagará de multa por sua infração: R${:.2f}! '.format(multa))
 else:
     print('Você está dentro do limite de velocidade. Parabéns!')
