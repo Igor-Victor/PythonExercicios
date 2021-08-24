f83e68a7952e0598dc6155245c98ea0aef16f6ef
Igor-Victor <89404522+igor-victor@users.noreply.github.com>
2021-08-23 21:14:24 -0300

    Criada uma linha de texto.

---
+++ ex027.py
@@ -0,0 +1,13 @@
+#print('Exercício 27: Analisador de nome')
+#nome = str(input('Insira seu nome completo por favor: ')).strip()
+#n = nome.split()
+# mostrar primeiro nome
+#print('Seu primeiro nome é: "{}". '.format(n[0].capitalize()))
+# mostrar último nome
+#print('Seu último nome é: "{}". '.format(n[len(n)-1].capitalize()))
+
+print('Exercício 28: Analisador de nomes')
+nome = str(input('Olá! Digite o seu nome completo por favor: '))
+n = nome.split()
+print('Seu primeiro nome é: "{}". '.format(n[0].capitalize()))
+print('Seu segndo nome é: "{}". '.format(n[len(n)-1]))
