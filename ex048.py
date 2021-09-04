057aa899ab62088468e7a32eebc4aba4d4e01aa7
Igor Victor <contatoigorvictor@gmail.com>
2021-09-04 18:03:31 -0300

    Soma entre números ímpares e multíplos de 3 1/500

--- ex048.py
+++ ex048.py
@@ -4,4 +4,4 @@ s = 0
 for n in range(0, 501, 3):
     if n % 2 != 0:
         s += n
-print('A soma dos multiplos ímpares de 3 entre 1 e 500 é: {}. '.format(s))
+print('\nA soma dos multiplos ímpares de 3 entre 1 e 500 é: {}. '.format(s))
